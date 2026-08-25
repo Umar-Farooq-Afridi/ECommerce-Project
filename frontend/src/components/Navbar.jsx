57361c788f2465243f31f42a618bcf3b41c4aac0
Umar-Farooq-Afridi <umerfarooq21441@gmail.com>
2026-08-25 15:45:26 +0500

    Create add to cart functionality and make cart icon value dynamic

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch } = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-around py-5 font-medium">
@@ -60,9 +60,9 @@ const Navbar = () => {
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <div
             className="absolute right-[-5px] bottom-[-5px] w-4 text-center 
-                    leading-4 bg-black text-white aspect-square rounded-full text-[8px]"
+              leading-4 bg-black text-white aspect-square rounded-full text-[8px]"
           >
-            10
+            {getCartCount()}
           </div>
         </Link>
 
